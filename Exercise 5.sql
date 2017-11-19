30906b58866645ba03eb93b80959643bae21c54c
Saurabh Jain <28838017+sjain507@users.noreply.github.com>
2017-11-19 23:55:15 +0530

    Formatted Exercise 5.sql

---
+++ Exercise 5.sql
@@ -0,0 +1,5 @@
+#5. Find the model number, speed and hard drive capacity of PCs cheaper than $600 having a 12x or a 24x CD drive.
+
+Select model, speed, hd
+from PC
+where price < 600 and  (cd = '12x' or cd= '24x')
